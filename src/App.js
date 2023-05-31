c63b85b3f95926a369862158cea84bd07df18624
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2023-05-31 07:51:10 +0700

    Part1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import ParticlesBg from "particles-bg";
+import "./App.css";
+import { BsGithub } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="card">
+        <div className="card_body">
+          <div className="profile text-center">
+            <img src="/logo192.png" className="avatar" alt="avatar" />
+            <div className="bg_content rd_12 p_8">
+              <h1>Ahmad Abu Hasan</h1>
+              <p>Android Developer</p>
+            </div>
+          </div>
+          <div className="mt-16">
+            <a
+              className="btn_action bg_content"
+              href="https://github.com/eby8zevin"
+            >
+              <BsGithub />
+              <span>GitHub</span>
+            </a>
+          </div>
+          <div className="mt-16">
+            <a
+              className="btn_action bg_content"
+              href="https://www.instagram.com/eby8zevin"
+            >
+              <BsInstagram />
+              <span>Instagram</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <ParticlesBg type="thick" bg={true} />
     </div>
   );
 }
